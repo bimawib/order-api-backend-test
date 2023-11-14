537c8deef93a2c1e571797f5ed953c7c5854a7bb
Bima Wib <bimawib@eskalink.com>
2023-11-14 17:18:04 +0700

    feat(api): add show method to product api

--- src/main/java/com/bimawib/orderapi/controller/ProductController.java
+++ src/main/java/com/bimawib/orderapi/controller/ProductController.java
@@ -1,13 +1,16 @@
 package com.bimawib.orderapi.controller;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +29,7 @@ public class ProductController {
 		if(products.size() > 0) {
 			return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
 		} else {
-			return new ResponseEntity<>("Product's Data Not Found", HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>("Product's data not found!", HttpStatus.NOT_FOUND);
 		}
 	}
 	
@@ -41,6 +44,27 @@ public class ProductController {
 			return new ResponseEntity<Product>(product, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping("/product/{id}")
+	public ResponseEntity<?> show(@PathVariable("id") String id){
+		Optional<Product> productResult = productRepository.findById(id);
+		if(productResult.isPresent()) {
+			return new ResponseEntity<>(productResult.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("Product with id: " + id + " not found!", HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@PutMapping("/product/{id}")
+	public ResponseEntity<?> update(@PathVariable("id") String id, @RequestBody Product product){
+		Optional<Product> productResult = productRepository.findById(id);
+		if(productResult.isPresent()) {
+			Product productToUpdate = productResult.get();
+			productToUpdate.setCompleted(product.getCompleted() != null ? product.getCompleted());
+		} else {
+			return new ResponseEntity<>("Product with id: " + id + " not found!", HttpStatus.NOT_FOUND);
 		}
 	}
 	
